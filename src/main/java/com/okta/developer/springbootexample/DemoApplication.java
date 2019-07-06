f71fd3598206b18cfd5b5770842887a76acaee21
ukanabar <utkarsh_kanabar39@yahoo.com>
2019-07-06 00:06:02 -0400

    Added space to see if build kick in or not

--- src/main/java/com/okta/developer/springbootexample/DemoApplication.java
+++ src/main/java/com/okta/developer/springbootexample/DemoApplication.java
@@ -3,7 +3,7 @@ package com.okta.developer.springbootexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  
 public class DemoApplication {
 
 	public static void main(String[] args) {
